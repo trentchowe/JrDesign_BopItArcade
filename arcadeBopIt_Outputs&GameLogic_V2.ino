882f83a3e617e1e6edf9a140ffe78859d99f01bc
WillD08 <97646833+WillD08@users.noreply.github.com>
2023-03-21 22:17:42 -0400

    Add files via upload

--- arcadeBopIt_Outputs&GameLogic_V2.ino
+++ arcadeBopIt_Outputs&GameLogic_V2.ino
@@ -1,23 +1,30 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "SoftwareSerial.h"
-#include "DFRobotDFPlayerMini.h"
+//#include "DFRobotDFPlayerMini.h"
 
 //lcd I2C pins declaration
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-// //MP3 setup
-// // Use pins 2 and 3 to communicate with DFPlayer Mini
-// static const uint8_t PIN_MP3_TX = 2; // Connects to module's RX 
-// static const uint8_t PIN_MP3_RX = 3; // Connects to module's TX 
-// SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
+//MP3 setup
+// Use pins 2 and 3 to communicate with DFPlayer Mini
+//static const uint8_t PIN_MP3_TX = 1; // Connects to module's RX
+//static const uint8_t PIN_MP3_RX = 0; // Connects to module's TX
+//SoftwareSerial softwareSerial(0, 1);
+SoftwareSerial mySerial(1, 0);
 
-// const int wirePin = 4; 
-// const int endPin = 5; 
+// const int wirePin = 4;
+// const int endPin = 5;
 
 // // Create the Player object
-// DFRobotDFPlayerMini player;
-
+//DFRobotDFPlayerMini player;
+#define Start_Byte 0x7E
+#define Version_Byte 0xFF
+#define Command_Length 0x06
+#define End_Byte 0xEF
+#define Acknowledge 0x00
+#define ACTIVATED LOW
+ 
 //TODO
 #define coinslotPin 9 //defining the pin for starting a new game to digital pin 9 (pin 15 of ATMega328P), this cannot
 //be the reset pin or else the initial start of the game (when the device is first turned on) cannot occur
@@ -30,6 +37,7 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
 int lastStateCLK; //value (0 or 1) of previous state of Output A of rotary encoder
 //making this a global variable (outside of setup()) because it needs to be accessed by multiple functions
+int currentStateCLK;
 int roundNum = 0;
 int xValue = 0; // To store value of the X axis
 int yValue = 0; // To store value of the Y axis
@@ -39,13 +47,12 @@ bool started = false;
 
 void setup() {
   // put your setup code here, to run once:
-	
+
   //pulling CLK and DT high for default state of rotary encoder because the metal pads of the rotary encoder pull its output signal low (0)
   //when turning the rotary encoder
   pinMode(CLK,INPUT_PULLUP);
   pinMode(DT,INPUT_PULLUP);
-  //read in the initial state of CLK (Output A) which should be 1 if it's not being turned due to its internal pullup resistor 
-  double lastStateCLK = digitalRead(CLK);
+  //read in the initial state of CLK (Output A) which should be 1 if it's not being turned due to its internal pullup resistor
   pinMode(9, INPUT_PULLUP);
   //setup LCD screen
   lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
@@ -53,16 +60,17 @@ void setup() {
   lcd.clear();//Clean the screen
   lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
   lcd.print("Press to Start");
-	
+
   //MP3 player
   // Init USB serial port for debugging
   Serial.begin(9600);
-  // // Init serial port for DFPlayer Mini
+  // Init serial port for DFPlayer Mini
   //softwareSerial.begin(9600);
+  lastStateCLK = digitalRead(CLK);
 
   // // Start communication with DFPlayer Mini
   // if (player.begin(softwareSerial)) {
-	// Serial.println("OK");
+	// Serial.println("connecting to mp3");
 
 	// // Set volume to maximum (0 to 30).
 	// player.volume(30);
@@ -71,21 +79,26 @@ void setup() {
   // } else {
   // 	Serial.println("Connecting to DFPlayer Mini failed!");
   // }
+  mySerial.begin (9600);
+  //delay(1000);
+  //playFirst();
 
 }
 
 void loop() {
   //Serial.println("Loop");
   // put your main code here, to run repeatedly:
+  
     if(checkCoinslot()){
       started = true;
       Serial.println("Game Start");
     }
     if(started)
     {
+      //lcd.clear();//Clean the screen
       //4 actions (turn right, turn left, honk it, shift it)
-      int action = random(1, 4);
-      bool completed = 0; //checking if a proper action is done, initially this is 0 (false) 
+      int action = random(1, 5);
+      bool completed = 0; //checking if a proper action is done, initially this is 0 (false)
 
       switch (action)
       {
@@ -104,24 +117,37 @@ void loop() {
           completed = checkAction4(5 - (.045 * roundNum));
           break;
       }
-      
+
       if(completed)//check if completed is true
       {
         //can add playVictorySound() if we have time, not required
-        displayroundNumCount();
+        Serial.println("You succeeded: Calling display round num count function.");
         roundNum++;
+        if (roundNum == 100){
+          lcd.clear();//Clean the screen
+          lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+          lcd.print(" You Won! ");
+        }
+        displayroundNumCount();
       }
       else{
+        lcd.clear();//Clean the screen
+        lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+        lcd.print(" Good Try! ");
+        Serial.println("You failed: Calling display round num count function.");
+        delay(1000);
         displayroundNumCount();
         roundNum = 0;
         started = false;
+        lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+        lcd.print("Press to Restart");
       }
     }
 }
 
 
 bool checkCoinslot()
-//checks if the SPST switch has been pushed meaning a new game should begin 
+//checks if the SPST switch has been pushed meaning a new game should begin
 {
     int coinslotState = digitalRead(coinslotPin);
     if(coinslotState == LOW)
@@ -134,35 +160,51 @@ bool checkCoinslot()
     }
 }
 
-bool checkAction1(int time) 
+bool checkAction1(int time)
 //"Turn Right" steering wheel turn action
 {
     Serial.println("Turn Right!");
+    
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Turn Right! ");
+    playSpecific(4);
     //play sound ("Turn Right" maybe?)
     //then check for input
     double startTime = millis(); //number of ms since Arduino program began running
     double endTime = startTime;
-    int Turn = TurnIt();
+    int Turn;
+    lastStateCLK = digitalRead(CLK);
     while((endTime - startTime) <= (time*1000)) //time is in milliseconds so multiply seconds (time) by 1000
     {
-
-      // if(Turn == 1)
-      // {
-      //   Serial.println("Correct!");
-      //   return true;
-      // }
-      // endTime = millis();
+       Turn = TurnIt();
+       
+       if(Turn == 1)
+       {
+         Serial.println("Correct!");
+         Serial.println(Turn);
+         return true;
+       }
+       endTime = millis();
     }
     Serial.println("Out of Time!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Wrong move! ");
     return false;
 }
 
 bool checkAction2(int time)
 //turn left
 {
-    //play sound ("Turn Left" maybe?)
+    //play sound ("Turn Left")
+    
     //then check for input
     Serial.println("Turn Left!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Turn Left! ");
+    playSpecific(4);
     double startTime = millis(); //number of ms since Arduino program began running
     double endTime = startTime;
     //variable to keep track if rotary encoder was turned
@@ -171,15 +213,19 @@ bool checkAction2(int time)
     {
       //getting the value from TurnIt function to see if it is turned left (2)
       Turn = TurnIt();
-      Serial.println(Turn);
+     // Serial.println(Turn);
       if(Turn == 2)
       {
         Serial.println("Correct!");
+        Serial.println(Turn);
         return true;
       }
       endTime = millis();
     }
     Serial.println("Out of Time!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Wrong move! ");
     return false;
 }
 
@@ -187,96 +233,116 @@ bool checkAction3(int time)
 //honk horn
 {
     Serial.println("Honk Horn!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Honk Horn! ");
+    playSpecific(2);
+    
     //play sound
 
     //then check for input
     double startTime = millis();
-    double endTime = startTime;
+    //double endTime = startTime;
 
-    double currentStateCLK = digitalRead(CLK);
-    lastStateCLK = currentStateCLK;
+    //double currentStateCLK = digitalRead(CLK);
+    //lastStateCLK = currentStateCLK;
     int btnState = digitalRead(SW);
 
-    while((endTime - startTime) <= (time*1000)){ //time is in milliseconds
+    while((millis() - startTime) <= (time*1000)){ //time is in milliseconds
+      btnState = digitalRead(SW);
       if(btnState == HIGH){
         Serial.println("Correct!");
+        playSpecific(1);
         return true;
       }
-      endTime = millis();
+      //endTime = millis();
     }
     Serial.println("Out of Time!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Wrong move! ");
     return false;
-    
+
 }
 
 bool checkAction4(int time)
 //move joystick
 {
   Serial.println("Move Joystick!");
+  lcd.clear();//Clean the screen
+  lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+  lcd.print("Move Joystick!");
+  playSpecific(3);
+  
     //play sound
 
     //then check for input
     double startTime = millis();
-    double endTime = startTime;
+    //double endTime = startTime;
     int moved = JoystickMove();
-    while((endTime - startTime) <= (time*1000)){ //time is in milliseconds
+    while((millis() - startTime) <= (time*1000)){ //time is in milliseconds
+      moved = JoystickMove();
       if(moved == 1)
       {
         Serial.println("Correct!");
         return true;
       }
-      endTime = millis();
+      //endTime = millis();
     }
     //return false because the time ran out
     Serial.println("Out of Time!");
+    lcd.clear();//Clean the screen
+    lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
+    lcd.print(" Wrong move! ");
     return false;
-    
+
 }
+
 void displayroundNumCount() {
+  Serial.println("Display round num count");
   lcd.clear();//Clean the screen
   lcd.setCursor(0,0); //Defining positon to write from, first row,first column .
   lcd.print("round: ");
-  delay(1000);
+  delay(500);
   lcd.print(roundNum);
-  delay(2000);
-  lcd.clear();  //clear again after use	
+  delay(1000);
+  lcd.clear();  //clear again after use
 }
 
 int TurnIt()
 //function that checks if the steering wheel is turned left or right
 //returns 1 if turned right, 2 if turned left, and 3 if no turn has been received
 {
-  int currentStateCLK;
   // Read the current state of CLK (output A)
 	currentStateCLK = digitalRead(CLK);
   //if the state of Output A has changed (currentStateCLK != lastStateCLK) and output A is currently a 0 (currentStateCLK == 0) (so we are only checking when Output A is on a metal pad (0))
   if (currentStateCLK != lastStateCLK  && currentStateCLK == 0)
   {
-    // If the DT state is different than the CLK state and CLK state is 0 (low), and since DT != CLK at this time, 
+    // If the DT state is different than the CLK state and CLK state is 0 (low), and since DT != CLK at this time,
     //that means we are CW because output A recevied a signal first meaning it touched a metal pad (pulling its value to ground, 0) first in the rotary encoder
     //and output A is closest to the metal pad in the clockwise direction (***And the outputs A and B are stationary, and we are rotating the metal pads of the rotary encoderthat can come into contact with outputs A and B)
-		if (digitalRead(DT) != currentStateCLK) 
+		if (digitalRead(DT) != currentStateCLK)
     {
-      Serial.println("1");      
-      //return 1 becasue rotary encoder has turned right (CW) 
+      //Serial.println("1");
+      //return 1 becasue rotary encoder has turned right (CW)
 			return 1;
     }
-    //this means that we are rotating counterclockwise because when Output B is equal to Output A, and we already confirmed that output A is a 0, this means 
+    //this means that we are rotating counterclockwise because when Output B is equal to Output A, and we already confirmed that output A is a 0, this means
     //that we have rotated counterclockwise, and Output B has already gotten onto the conductive pad (became a 0) before output A, meaning Output B was pulled to 0 before output A
     else if (digitalRead(DT) == currentStateCLK)
     // return 2 becasue rotary encoder has turned left (CCW)
-    Serial.println("2");
+    //Serial.println("2");
 	  	return 2;
   }
   //return 3 because no turn was detected
-  Serial.println("3");
+  //Serial.println("3");
   return 3;
 }
 
 bool Honk()
 {
   int btnState = digitalRead(SW);
-  if (btnState == HIGH) 
+  if (btnState == HIGH)
   {
   Serial.println("Button pressed!");
   //return 1 because button pressed
@@ -295,11 +361,42 @@ bool JoystickMove()
   yValue = analogRead(VRY_PIN);
   if (xValue <= 258 || yValue <= 252 || xValue >= 770 || yValue >= 779)
   {
-    //return true because the joystick was moved properly  
+    //return true because the joystick was moved properly
     return true;
   }
 //return false because the joystick was not moved enough
 return false;
 	//debouncing delay
-  delay(100);	
+  delay(100);
+}
+
+void playSpecific(int num)
+{
+  execute_CMD(0x3F, 0, 0);
+  delay(1);
+  setVolume(20);
+  delay(500);
+  execute_CMD(0x03,0, num); 
+  delay(500);
+}
+
+void setVolume(int volume)
+{
+  execute_CMD(0x06, 0, volume); // Set the volume (0x00~0x30)
+  delay(1);
+}
+
+void execute_CMD(byte CMD, byte Par1, byte Par2)
+// Excecute the command and parameters
+{
+// Calculate the checksum (2 bytes)
+word checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2);
+// Build the command line
+byte Command_line[10] = { Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
+Par1, Par2, highByte(checksum), lowByte(checksum), End_Byte};
+//Send the command line to the module
+for (byte k=0; k<10; k++)
+{
+mySerial.write( Command_line[k]);
+}
 }
